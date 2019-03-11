7aa0a0c1ccc3135caa55e5f602f7b6ff7db14ee4
arohithr8 <46582843+arohithr8@users.noreply.github.com>
2019-03-11 04:44:49 -0500

    v3

--- SeleniumNUnitParam/BrowserTest.cs
+++ SeleniumNUnitParam/BrowserTest.cs
@@ -15,8 +15,9 @@ namespace SeleniumNUnitParam
         [Test]
         public void GoogleTest()
         {
+            IWebDriver driver = new ChromeDriver();
             //IWebDriver driver = new ChromeDriver("C:\tools\selenium\chromedriver.exe");
-            IWebDriver driver = new ChromeDriver(@”C:\tools\selenium\chromedriver.exe”);
+            //IWebDriver driver = new ChromeDriver(@”C:\tools\selenium\chromedriver.exe”);
             Driver.Navigate().GoToUrl("http://www.google.com");
             Driver.FindElement(By.Name("q")).SendKeys("Selenium");
             System.Threading.Thread.Sleep(5000);
@@ -29,6 +30,7 @@ namespace SeleniumNUnitParam
         [Test]
         public void ExecuteAutomationTest()
         {
+            IWebDriver driver = new ChromeDriver();
             //IWebDriver driver = new ChromeDriver("C:\tools\selenium\chromedriver.exe");
             IWebDriver driver = new ChromeDriver(@”C:\tools\selenium\chromedriver.exe”);
             Driver.Navigate().GoToUrl("http://executeautomation.com/demosite/Login.html");
